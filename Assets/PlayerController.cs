d8d0b685527f66ca4812398581eb899a7388225d
Brad Stapleton <bradstap@hotmail.co.uk>
2019-01-25 13:07:27 +0000

    Player Movement and shooting started

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float rotSpeed = 1;
+    public float fireDelay = 0.5f;
+    public Transform cannon;
+    public GameObject shotPrefab;
+    public PlayerPerspective perspective = PlayerPerspective.TopDown;
+
+    private float tToNextShot = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +21,32 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        switch (perspective)
+        {
+            case PlayerPerspective.TopDown:
+                TopDownUpdate();
+                break;
+            case PlayerPerspective.Side:
+                break;
+            case PlayerPerspective.FromBehind:
+                break;
+            default:
+                break;
+        }
+
+
+        if (Input.GetAxis("Fire1") > 0 && tToNextShot <= 0)
+        {
+            tToNextShot = fireDelay;
+            Transform bullet = Instantiate(shotPrefab, cannon.position, cannon.rotation).transform;
+            bullet.GetComponent<MoveBasic>().direction = cannon.up;
+        }
+
+        tToNextShot -= Time.deltaTime;
+    }
+
+    void TopDownUpdate()
     {
         Vector3 rotationTarget = new Vector3();
         rotationTarget.y = -90;
@@ -33,4 +65,11 @@ public class PlayerController : MonoBehaviour
         Quaternion targetQuat = Quaternion.Euler(rotationTarget);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetQuat, Time.deltaTime * rotSpeed);
     }
+}
+
+public enum PlayerPerspective
+{
+    TopDown,
+    Side,
+    FromBehind
 }
